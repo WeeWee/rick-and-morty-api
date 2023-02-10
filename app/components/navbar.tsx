6fd9666e6cadfa402d4a9522b48f6e1c54acc14c
IndieAK <kindbergadam@gmail.com>
2023-02-10 15:34:18 +0100

    final commit

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,20 +1,21 @@
 import { Form, Link } from "@remix-run/react";
 import { useRef } from "react";
-import { HomeIcon } from "@heroicons/react/24/outline";
 export const Navbar = () => {
-	const formRef = useRef(null);
+  const formRef = useRef(null);
 
-	return (
-		<nav className="navbar">
-			<h1>
-				<Link to="/">The Best Rick and Morty Website</Link>
-			</h1>
-
-			<Form ref={formRef} method="get" action="/results" id="search-form">
-				<input name="page" hidden defaultValue="1" />
-				<input name="name" placeholder="search name" />
-				<button type="submit">Search</button>
-			</Form>
-		</nav>
-	);
+  return (
+    <nav className="navbar">
+      <h1>
+        <Link to="/">The Best Rick and Morty Website</Link>
+      </h1>
+      <h4>
+        <Link to="/">Home</Link>
+      </h4>
+      <Form ref={formRef} method="get" action="/results" id="search-form">
+        <input name="page" hidden defaultValue="1" />
+        <input name="name" placeholder="search name" />
+        <button type="submit">Search</button>
+      </Form>
+    </nav>
+  );
 };
